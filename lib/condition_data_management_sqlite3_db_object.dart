1faf8e43f0d6210dc15a5f9f54297a94c89bcf10
brilliapps <c.slaw@wp.pl>
2023-06-26 11:01:12 +0200

    Added missing two files (i've forgotten)

---
+++ lib/condition_data_management_sqlite3_db_object.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:ffi';
+import 'package:sqlite3/sqlite3.dart';
+import 'condition_configuration.dart';
+import 'condition_data_management_driver_sql_settings.dart';
+
+class Sqlite3DB {
+  Sqlite3DB() {
+    // now windows only
+  }
+
+  static Future<dynamic> getDBObject(dynamic settings_sqlite3) {
+    if (settings_sqlite3 == null ||
+        settings_sqlite3 is! ConditionDataManagementDriverSqlSettingsSqlite3?) {
+      throw Exception(
+          '[Sqlite3DB] native platform version class exception: settings_sqlite3 is! ConditionDataManagementDriverSqlSettingsSqlite3?');
+    }
+    Completer<dynamic> completer = Completer<dynamic>();
+    scheduleMicrotask(() {
+      DynamicLibrary.open(ConditionConfiguration
+          .paths_to_sqlite_core_library[ConditionPlatforms.Windows]);
+      dynamic db = sqlite3.open(settings_sqlite3!
+          .native_platform_sqlite3_db_paths[ConditionPlatforms.Windows]!);
+      completer.complete(db);
+    });
+    return completer.future;
+  }
+}
---
+++ lib/condition_data_management_sqlite3_db_object.web.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart'
+    as http; // originally imported here with 'package:sqlite3/wasm.dart' in mind
+import 'package:sqlite3/wasm.dart';
+
+class Sqlite3DB {
+  Sqlite3DB() {
+    // now windows only
+  }
+
+  static Future<dynamic> getDBObject(dynamic settings_sqlite3) async {
+    if (settings_sqlite3 != null) {
+      throw Exception(
+          '[Sqlite3DB] web platform version class exception: settings_sqlite3 is! ConditionDataManagementDriverSqlSettingsSqlite3?');
+    }
+    final response = await http.get(Uri.parse('sqlite3.wasm'));
+
+    // The [IndexedDbFileSystem] class is descentant of [VirtualFileSystem] which will be needed in a while
+    IndexedDbFileSystem fs = await IndexedDbFileSystem.open(dbName: 'test');
+    WasmSqlite3 wasmSqlite3Object = await WasmSqlite3.load(response.bodyBytes);
+
+    // So here as a param wee need to pass the mentioned [VirtualFileSystem] object and [IndexedDbFileSystem] is descentant of the class [VirtualFileSystem]
+    wasmSqlite3Object.registerVirtualFileSystem(fs);
+
+    dynamic db = wasmSqlite3Object.open('testsname.sqlite3');
+
+    return db;
+  }
+}
